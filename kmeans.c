8dd7e115bca0b8d1b101d5b595f61e76e01351d6
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-04-20 17:11:25 +0300

    compiled!

--- kmeans.c
+++ kmeans.c
@@ -1,35 +1,42 @@
 #include <stdio.h>
 #include <assert.h>
+void *calloc(size_t nitems, size_t size);
+char *strtok(char * str, const char *delim);
+double atof(const char * str);
 
 int main(int argc, char *argv[]) {
-    int k = argv[0];
-    int max_iter = 200;
-    char *input_path;
+    int k, max_iter, d, N, i;
+    char *input_path, buffer[1000], *point, ch;
+    float *vector;
+    FILE *input_file;
+
+    sscanf(argv[0], "%d", &k);
+    max_iter = 200;
     if (argc == 3) {
-        max_iter = argv[1];
+        sscanf(argv[1], "%d", &max_iter);
         input_path = argv[2];
     }
     else {
         input_path = argv[1];
     }
 
-    FILE *input_file = fopen(input_path,'rb');
-    char buffer[1000];
-    char *point;
-    char ch;
-    int d;
+    input_file = fopen(input_path,"r");
+    
     while (fgets(buffer,1000,input_file) != NULL) {   
-        for (int i = 0; i < 1000; i++) {
+        N++;
+    }
+    while (fgets(buffer,1000,input_file) != NULL) {   
+        for (i = 0; i < 1000; i++) {
             if (buffer[i] == ',') {
                 d++;
             }
         }
-        float vector[d+1];
+        vector = (float *)calloc(d, sizeof(float));
         
-        for (int i = 0; i < d+1; i++) {
-            point = strtok(buffer, ',');
+        for (i = 0; i < d+1; i++) {
+            point = strtok(buffer, ",");
             vector[i] = atof(point);
         }
     }
-
+return 0;
 }
